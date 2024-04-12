2cf53dbda556b5cd016b7bf0688ffb96dc7a2597
KTisKIM <ktkim2k1@paran.com>
2024-04-12 18:20:35 -0400

    04/12/2024 - Added the optional call, get_score()

--- api.py
+++ api.py
@@ -2,6 +2,11 @@ import requests
 import json
 import keys
 
+#########################################
+#####              API              #####
+#####   - API is case sensitive.    #####
+#####   - e.g. teamid != teamId     #####
+#########################################
 
 def enter_world(world_id):
 
@@ -37,7 +42,51 @@ def locate_me():
         keys.WORLD_URL, headers=headers, data=payload, params=params
     )
     print(response.text)
+    
 
+def get_score(team_id):
+    """
+    Request Type: GET
+
+    Parameters: type=score, teamId=$teamId
+    Return Values: score. Fails if you are not in the team (you can only get scores for your team).
+    
+    ***** This call is entirely optional and will be useful only after many runs have been completed.
+    ***** Your program never needs to make this call.
+    """
+
+    payload = {}
+    params = {"type": "score", "teamId": team_id}
+    headers = {
+        "x-api-key": keys.API_KEY,
+        "userId": keys.USER_ID,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": "PostmanRuntime/7.37.0",
+    }
+
+    response = requests.get(keys.SCORE_URL, headers=headers, data=payload, params=params)
+    # print("DEBUG:", response.text)  # Example Result of Success: {"score":0,"code":"OK"}
+                                      # Example Result of Fail: {"code":"FAIL","message":"Invalid team ID, or you are not in the team"}
+    response_in_dict = json.loads(response.text)
+    # print("DEBUG:", response_in_dict)  # Example: {'score': 0, 'code': 'OK'}
+
+    if response_in_dict["code"] == "OK":
+        score = response_in_dict["score"]
+        # print("DEBUG:", score)
+        return score
+    elif response_in_dict["code"] == "FAIL":  # Fails if the team ID is invalid or if you are not in the team
+        fail_message = response_in_dict["message"]
+        print(fail_message)  # Example: Invalid team ID, or you are not in the team
+    else:
+        print("*** ERROR ***")
+
+
+
+#########################################
+#####         Testing Calls         #####
+#########################################
 
 # enter_world(0)
-locate_me()
+# locate_me()
+
+get_score(team_id=1397)
