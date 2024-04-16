fc8661390344cf95c7158a25e95f9a0c6d84a489
Arsalan Bin Najeeb <38084250+anw10@users.noreply.github.com>
2024-04-16 19:57:05 -0400

    loads mistake

--- play.py
+++ play.py
@@ -5,29 +5,27 @@ import time
 
 def full_algo():
 
-    api.enter_world("0")
-
     Q = agent.load_json("Q.json")
-
     N = agent.load_json("N.json")
 
-    prev_state = None
-    prev_action = None
-
-    current_pos = api.locate_me()
-    reward = 0
-
-    terminal_state = True
-
     for i in range(0, 100):
-        while terminal_state:
+
+        # Each epoch, enter the world
+        api.enter_world("0")
+
+        prev_state = None
+        prev_action = None
+        current_pos = api.locate_me()
+        reward = 0
+
+        while current_pos:
             next_action, Q, N = agent.q_learning_agent(
                 prev_state, prev_action, current_pos, reward, Q, N, 0.9
             )
             prev_state = current_pos
             prev_action = next_action
 
-            reward = json.loadsapi.make_move()
+            reward = api.make_move()
 
             current_pos = api.locate_me()
 
